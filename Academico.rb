7f1357ec1efc68263cd58a238e9980181ac832a7
pemk15 <pemk15@inf.ufpr.br>
2020-12-21 15:07:12 -0300

    Starting main

--- Academico.rb
+++ Academico.rb
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 $:.push './'
 require 'professor.rb'
+require 'aluno.rb'
 
 # Método 2
 est = Professor.new ({:nome => "Lucca Pietro Benício Fogaça", :email => "luccapietrobeniciofogaca@policiacivil.sp.gov.br", :salario => "1921.45"})
+est.save
+
+# Método 2
+est = Aluno.new ({:cpf => "538.132.869-91",:nome => "Pedro Daniel Pinto", :email => "pedrodanielpinto_@pss.adv.br", :fone => "41 99536-0600", :data_nascimento => "19960820"})
 est.save
