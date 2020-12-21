66a28559ffb515eed1c04f0b1b3a3357316ee50e
pemk15 <pemk15@inf.ufpr.br>
2020-12-21 15:07:41 -0300

    Starting main

--- insereAlunos.rb
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-$:.push './'
-require 'aluno.rb'
-
-# Método 2
-est = Aluno.new ({:cpf => "538.132.869-91",:nome => "Pedro Daniel Pinto", :email => "pedrodanielpinto_@pss.adv.br", :fone => "41 99536-0600", :data_nascimento => "19960820"})
-est.save
