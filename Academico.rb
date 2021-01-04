6abeee8167c1966ea1dc57a6b2f18241982a9a86
pemk15 <pemk15@inf.ufpr.br>
2021-01-04 16:00:41 -0300

    insert working properly

--- Academico.rb
+++ Academico.rb
@@ -76,7 +76,7 @@ until stop_condition == line = gets
       words.shift;
       if op == 0 then
         columns = words.select {|e| e =~  /professors_id|cursos_id|data_eleicao/i};
-        if columns.size < 2 or columns.size > 2 then
+        if columns.size < 3 or columns.size > 3 then
           STDERR.puts "Devem ser informados os valores para as colunas: professors_id, cursos_id e data_eleicao"
         end
         fields = [{
@@ -113,36 +113,54 @@ until stop_condition == line = gets
         end
       end
     when words[0] =~ /matricula/i
-      classe = Matricula.new()
+      words.shift;
+      if op == 0 then
+        columns = words.select {|e| e =~  /turmas_id|alunos_id|data_matricula/i};
+        if columns.size < 3 or columns.size > 3 then
+          STDERR.puts "Devem ser informados os valores para as colunas: turmas_id, alunos_id e data_matricula"
+        end
+        fields = [{
+          :"#{columns[0].downcase}"=>words[(words.index(columns[0])+1)..(words.index(columns[1])-1)].join(" "),
+          :"#{columns[1].downcase}"=>words[(words.index(columns[1])+1)..(words.index(columns[2])-1)].join(" "),
+          :"#{columns[2].downcase}"=>words[(words.index(columns[2])+1)..-1].join(" ")
+        }];
+
+        fields.each do |e|
+          matricula = Matricula.new()
+          matricula.turmas_id = e[:turmas_id]
+          matricula.alunos_id = e[:alunos_id]
+          matricula.data_matricula = e[:data_matricula]
+          matricula.save
+        end
+      end
     when words[0] =~ /turma/i
-      classe = Turma.new()
+      words.shift;
+      if op == 0 then
+        columns = words.select {|e| e =~  /professors_id|cursos_id|data_inicio|carga_horaria/i};
+        if columns.size < 4 or columns.size > 4 then
+          STDERR.puts "Devem ser informados os valores para as colunas: professors_id, cursos_id, data_inicio e carga_horaria"
+        end
+        fields = [{
+          :"#{columns[0].downcase}"=>words[(words.index(columns[0])+1)..(words.index(columns[1])-1)].join(" "),
+          :"#{columns[1].downcase}"=>words[(words.index(columns[1])+1)..(words.index(columns[2])-1)].join(" "),
+          :"#{columns[2].downcase}"=>words[(words.index(columns[2])+1)..(words.index(columns[3])-1)].join(" "),
+          :"#{columns[3].downcase}"=>words[(words.index(columns[3])+1)..-1].join(" ")
+        }];
+
+        fields.each do |e|
+          turma = Turma.new()
+          turma.professors_id = e[:professors_id]
+          turma.cursos_id = e[:cursos_id]
+          turma.data_inicio = e[:data_inicio]
+          turma.carga_horaria = e[:carga_horaria]
+          turma.save
+        end
+      end
     else
       STDERR.puts "#{words[0]} não é uma tabela ou relação válida"
       error = 1
   end
-  words.shift 
-
-  # words.each do |word|
-  #   count += 1
-  # end
 end
-
-puts(count)
-
-=begin
-inp = gets
-novo = ''
-
-str = ''
-inp.each_char do |n|
-  c = n
-  str = str + n
-  if n == ',' or n == '\n' then
-    puts str
-  end
-end
-puts str
-=end
 
 =begin
 if startsWith (ins, rem, atu) then
@@ -165,20 +183,4 @@ if startsWith (ins, rem, atu) then
 else
   raise errorNotAValidOperation;
 end
-=end
-
-
-
-=begin
-findOperation() {
-  
-}
-
-# Método 2
-est = Professor.new ({:nome => "Lucca Pietro Benício Fogaça", :email => "luccapietrobeniciofogaca@policiacivil.sp.gov.br", :salario => "1921.45"})
-est.save
-
-# Método 2
-est = Aluno.new ({:cpf => "538.132.869-91",:nome => "Pedro Daniel Pinto", :email => "pedrodanielpinto_@pss.adv.br", :fone => "41 99536-0600", :data_nascimento => "19960820"})
-est.save
 =end
